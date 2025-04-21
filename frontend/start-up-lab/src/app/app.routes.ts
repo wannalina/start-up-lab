5de0524e4b203307a00ff43fd7ccc60e6af66383
Pawat Songkhopanit (Sun) <sunnypawat@gmail.com>
2025-04-21 17:28:20 +0200

    basic page setup

--- frontend/start-up-lab/src/app/app.routes.ts
+++ frontend/start-up-lab/src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: "/home", pathMatch: "full" },
+    { path: "home", component: HomeComponent },
+    { path: "aboutus", component: AboutusComponent},
+    { path: "contactus", component: ContactusComponent },
+    { path: "*", component: HomeComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
