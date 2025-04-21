fbabd281ac0df7ac04024aa5e17a562e5c996590
Pawat Songkhopanit (Sun) <sunnypawat@gmail.com>
2025-04-21 18:10:51 +0200

    basic login setup

--- frontend/start-up-lab/src/app/app.routes.ts
+++ frontend/start-up-lab/src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { ContactusComponent } from './contactus/contactus.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: "", redirectTo: "/home", pathMatch: "full" },
     { path: "home", component: HomeComponent },
-    { path: "aboutus", component: AboutusComponent},
+    { path: "aboutus", component: AboutusComponent },
     { path: "contactus", component: ContactusComponent },
-    { path: "*", component: HomeComponent },
+    { path: "login", component: LoginComponent }, // Added route for login
+    { path: "**", component: HomeComponent }, // Updated wildcard route
 ];
 
 @NgModule({
---
+++ frontend/start-up-lab/src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+    loginForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.loginForm = this.fb.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(6)]]
+        });
+    }
+
+    onSubmit(): void {
+        if (this.loginForm.valid) {
+            console.log('Form Submitted', this.loginForm.value);
+        } else {
+            console.log('Form is invalid');
+        }
+    }
+}
