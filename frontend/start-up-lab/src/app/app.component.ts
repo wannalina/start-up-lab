92f2e400555a6cb26834998a7271e0c6aeb79713
Pawat Songkhopanit (Sun) <sunnypawat@gmail.com>
2025-04-23 14:10:44 +0200

    setting up more modules

--- frontend/start-up-lab/src/app/app.component.ts
+++ frontend/start-up-lab/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HttpClientModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
