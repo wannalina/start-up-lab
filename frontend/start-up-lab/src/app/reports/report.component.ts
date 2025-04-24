6a132ab18cd023885ce6edde044c8a651f4b05ae
wannalina <annalina_wheeler@hotmail.com>
2025-04-24 14:52:29 +0200

    change format of report files & modify display of report data

--- frontend/start-up-lab/src/app/reports/report.component.ts
+++ frontend/start-up-lab/src/app/reports/report.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environment';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
     selector: 'app-report',
@@ -11,35 +12,37 @@ import { environment } from '../../../environment';
     styleUrls: ['./report.component.scss']
 })
 export class ReportComponent implements OnInit {
-    storyName: string = 'gameOne';
+    storyName: string = 'gameOne'; //! placeholder until game component supports this
     selectedReportName: string = '';
-    report: any;
+    report: any = {};
 
     constructor(private http: HttpClient) {}
 
     async ngOnInit() {
         this.selectedReportName = await this.getReportName();
-        this.report = this.loadReport();
+        this.report = await this.loadReport();
     }
 
     // function to fetch report name based on story name
     async getReportName() {
+        // fetch report name to display
         this.selectedReportName = await (await fetch(`${environment.serverApiUrl}/get-report?storyName=${this.storyName}`)).text();
-                        //.then(response => response.json());
-
-        console.log("report name:", this.selectedReportName);
         return this.selectedReportName;
     }
 
     // function to load report data based on game and report names
-    async loadReport() {
+    async loadReport(): Promise<Object | undefined>  {
+        // path to report json files
         const reportUrl = `../../assets/reports/${this.storyName}/${this.selectedReportName}.json`;
-        this.report = await this.http.get(reportUrl).toPromise();
-        console.log("Report:", this.report);
+        this.report = await firstValueFrom(this.http.get(reportUrl));
         return this.report;
     }
 
-    formatJsonLists() {
-
+    // function to convert report json object to string
+    formatJsonObject(jsonObject: Object): string {
+        const key = Object.keys(jsonObject)[0];
+        const value = (jsonObject as any)[key];
+        const objectToString = `${key}: ${value}`;
+        return objectToString;
     }
 }
